38e610505185f04c5537c64d749a71f4971ba750
Sofiane <sofianeboumelit@protonmail.ch>
2025-08-04 19:50:17 +0100

    challenge completed

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,9 +1,16 @@
 def square(number):
-    grains = 2*(number-1)
-    if(grains == 0):
-        return 1        
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    grains = 2**(number-1)       
     return grains 
 
 
 def total():
-    pass
+    try:
+        result = 0
+        for i in range(1, 65):
+            result += square(i)
+        return result
+    except Exception as exc:
+        raise ValueError(str(exc))
+    
