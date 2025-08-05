5e8c4103baf2878a05e05d29136a57284aec3a07
Sofiane <sofianeboumelit@protonmail.ch>
2025-08-05 15:02:16 +0100

    code review

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,15 +1,14 @@
 def square(number):
     if number < 1 or number > 64:
-        raise ValueError("square must be between 1 and 64")
+        raise ValueError("square must be between 1 and 64")    
     grains = 2**(number-1)       
+    
     return grains 
 
 
 def total():
     try:
-        result = 0
-        for i in range(1, 65):
-            result += square(i)
+        result = int(2*((1-square(64))/(1-2)))-1
         return result
     except Exception as exc:
         raise ValueError(str(exc))
