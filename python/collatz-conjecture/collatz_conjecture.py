3060b7636206b940e8f253f039bea5fc3fb97fa4
Sofiane <sofianeboumelit@protonmail.ch>
2025-08-08 19:49:45 +0100

    completed challenge

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,12 @@
 def steps(number):
-    pass
+    if not isinstance(number, int) or number < 1:
+        raise ValueError("Only positive integers are allowed")
+    value = number
+    counter = 0
+    while value != 1:
+        if value % 2 == 0:
+            value //= 2
+        else:
+            value = value * 3 + 1
+        counter += 1
+    return counter
