5bd1ab3ceec1b6c181c52492319c165d0290c0c9
Sofiane <sofianeboumelit@protonmail.ch>
2025-08-08 17:19:20 +0100

    completed challenge

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,7 @@
 def is_armstrong_number(number):
-    pass
+    if number <= 0:
+        return True
+    digits = [int(d) for d in str(number)]
+    n = len(digits)
+    armstrong_sum = sum(d ** n for d in digits)
+    return armstrong_sum == number
