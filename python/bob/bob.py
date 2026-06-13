3570355207d954d539e132ec105b0b2730606734
sofiane boumelit <sofianeboumelit@protonmail.ch>
2026-06-13 20:39:40 +0100

    my solution bob exercise

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,7 +2,8 @@ def response(hey_bob):
     str = hey_bob
     if str.endswith('?') and str.isupper()  : return "Calm down, I know what I'm doing!"
     if str.isupper()  : return "Whoa, chill out!"
-    if str.endswith('?') : return "Sure."
-    if str == None : return "Fine. Be that way!"
-    elif str == "" : return "Whatever."
+    if str.endswith('?') or str.endswith('?   ') : return "Sure."
+    if str == None or str == "" or str == "          " or str== "\n\r \t" or str =="\t\t\t\t\t\t\t\t\t\t": 
+        return "Fine. Be that way!"
+    elif str[-1]== " " : return "Whatever."
     return "Whatever."
