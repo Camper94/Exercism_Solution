dece0c9c3d65b1775f05e95f733dcf0c1b342a3a
sofiane boumelit <sofianeboumelit@protonmail.ch>
2026-06-13 21:13:54 +0100

    review my solution bob exercise

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,7 +1,7 @@
 def response(hey_bob):
     str = hey_bob.strip()
-    if str.endswith("?") and str.isupper()  : return "Calm down, I know what I'm doing!"
-    if str.isupper()  : return "Whoa, chill out!"
-    if str.endswith("?"): return "Sure."
     if not str: return "Fine. Be that way!"
+    if str[-1] == "?" and str.isupper()  : return "Calm down, I know what I'm doing!"
+    if str[-1] == "?": return "Sure."
+    if str.isupper()  : return "Whoa, chill out!"
     return "Whatever."
